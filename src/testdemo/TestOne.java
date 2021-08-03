c08e319325d2ed70ea24a7a02f8cd5713220d9d6
Ashique <Ashique@192.168.1.28>
2021-08-03 16:21:00 -0700

    This is my first commit.

---
+++ src/testdemo/TestOne.java
@@ -0,0 +1,15 @@
+package testdemo;
+
+public class TestOne {
+
+	public static void main(String[] args) {
+		String hungry = "Yes";
+		if (hungry == "Yes")
+			System.out.println("Eat something");
+		else {
+			System.out.println("Do your work");
+		}
+
+	}
+
+}

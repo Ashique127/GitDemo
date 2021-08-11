8b2f5cd3efc2330808a8a407ae416d599d254c22
Ashique127 <ashiqur.rahman2701@gmail.com>
2021-08-10 20:19:24 -0700

    Add this test two class

---
+++ src/testdemo/Testtwo.java
@@ -0,0 +1,15 @@
+package testdemo;
+
+public class Testtwo {
+
+	public static void main(String[] args) {
+		String hungry = "Yes";
+		if (hungry == "Yes")
+			System.out.println("Eat something");
+		else {
+			System.out.println("Do your work");
+		}
+
+	}
+
+}
